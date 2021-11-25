58a3b0dfd05b8b10fcd93e2b9c51ee4f7b20fbbd
mitriifd <idimka9@gmail.com>
2021-11-25 20:05:11 +0500

    counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
-    
+    const [count, setCount] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    const increment = () => {
+      setCount(count + 1)
+    }
+    const decrement = () => {
+      if (count) {
+        setCount(count - 1)
+      }
+    }
   
     return (
-      <>
-       
-      </>
+      <div className="App" style={{margin: '30px 0', textAlign: 'center'}}>
+        <button className="btn" onClick={decrement}>-</button>
+        <span style={{display: 'inline-block', padding: '0 30px', fontSize: '20px'}}>{count}</span>
+        <button className="btn" onClick={increment}>+</button>
+      </div>
     )
   
 }
