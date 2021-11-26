1a58eac492e502c73bdcc137c67ab30fc7bf0d3d
mitriifd <idimka9@gmail.com>
2021-11-26 16:44:08 +0500

    создание корзины

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,39 +3,42 @@ import { API_KEY, API_URL } from '../config';
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
 import { Cart } from './Cart';
+import { BasketList } from './BasketList';
 
 const Shop = () => {
 	const [goods, setGoods] = useState([]);
 	const [loading, setLoading] = useState(true);
-    const [order, setOrder] = useState([]);
+	const [order, setOrder] = useState([]);
+	const [isBasketShow, setBasketSow] = useState(false);
 
-    const addToBasket = (item) => {
-			const itemIndex = order.findIndex(
-				(orderItem) => orderItem.id === item.id
-			);
+	const addToBasket = (item) => {
+		const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
 
-			if (itemIndex < 0) {
-				const newItem = {
-					...item,
-					quantity: 1,
-				};
-				setOrder([...order, newItem]);
-			} else {
-				const newOrder = order.map((orderItem, index) => {
-					if (index === itemIndex) {
-						return {
-							...orderItem,
-							quantity: orderItem.quantity + 1,
-						};
-					} else {
-						return orderItem;
-					}
-				});
+		if (itemIndex < 0) {
+			const newItem = {
+				...item,
+				quantity: 1,
+			};
+			setOrder([...order, newItem]);
+		} else {
+			const newOrder = order.map((orderItem, index) => {
+				if (index === itemIndex) {
+					return {
+						...orderItem,
+						quantity: orderItem.quantity + 1,
+					};
+				} else {
+					return orderItem;
+				}
+			});
 
-				setOrder(newOrder);
-			}
-			
-		};
+			setOrder(newOrder);
+		}
+	};
+
+    const handleBasketShow = () => {
+		setBasketSow(!isBasketShow);
+	};
 
 	useEffect(function getGoods() {
 		fetch(API_URL, {
@@ -52,11 +55,18 @@ const Shop = () => {
 
 	return (
 		<main className='container content'>
-			<Cart quantity={order.length} />
+			<Cart quantity={order.length} handleBasketShow={handleBasketShow} />
 			{loading ? (
 				<Preloader />
 			) : (
 				<GoodsList goods={goods} addToBasket={addToBasket} />
+			)}
+			{isBasketShow && (
+				<BasketList
+					order={order}
+					handleBasketShow={handleBasketShow}
+					
+				/>
 			)}
 		</main>
 	);
