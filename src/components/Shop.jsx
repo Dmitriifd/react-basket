55574455928208badb4990d32e8c449831585e9f
mitriifd <idimka9@gmail.com>
2021-11-26 16:12:11 +0500

    функция добавления товара в заказ

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,10 +2,40 @@ import { useState, useEffect } from 'react';
 import { API_KEY, API_URL } from '../config';
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
+import { Cart } from './Cart';
 
 const Shop = () => {
 	const [goods, setGoods] = useState([]);
 	const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
+
+    const addToBasket = (item) => {
+			const itemIndex = order.findIndex(
+				(orderItem) => orderItem.id === item.id
+			);
+
+			if (itemIndex < 0) {
+				const newItem = {
+					...item,
+					quantity: 1,
+				};
+				setOrder([...order, newItem]);
+			} else {
+				const newOrder = order.map((orderItem, index) => {
+					if (index === itemIndex) {
+						return {
+							...orderItem,
+							quantity: orderItem.quantity + 1,
+						};
+					} else {
+						return orderItem;
+					}
+				});
+
+				setOrder(newOrder);
+			}
+			
+		};
 
 	useEffect(function getGoods() {
 		fetch(API_URL, {
@@ -20,13 +50,16 @@ const Shop = () => {
 			});
 	}, []);
 
-	return <main className='container content'>
-        {
-           loading ? <Preloader /> : <GoodsList goods={goods}/>
-        
-        }
-
-    </main>;
+	return (
+		<main className='container content'>
+			<Cart quantity={order.length} />
+			{loading ? (
+				<Preloader />
+			) : (
+				<GoodsList goods={goods} addToBasket={addToBasket} />
+			)}
+		</main>
+	);
 };
 
 export { Shop };
